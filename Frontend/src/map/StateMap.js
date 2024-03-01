6897a0422e9b4787936a7d51ddedff8eea3dfb95
Julie Su <julie.su@stonybrook.edu>
2024-03-01 01:55:44 -0500

    map

--- Frontend/src/map/StateMap.js
+++ Frontend/src/map/StateMap.js
@@ -1,4 +1,5 @@
 import { MapContainer, TileLayer, useMap, GeoJSON } from 'react-leaflet';
+import { useState, useEffect } from 'react';
 import 'leaflet/dist/leaflet.css';
 import './statemap.css';
 import MarylandState from "../Data/District_Boundaries/md_state.json";
@@ -8,10 +9,13 @@ import vaPopulation from "../Data/District_Boundaries/va_dummy_data.json"
 import Legend from './Legend';
 import legendItems from './LegendItems';
 
-const StateMap = ({selectedState, mapOptions }) => {
-    
-    var center, zoom;
 
+const StateMap = ({selectedState, mapOptions, selectedRace }) => {
+
+    const [layerRefs, setLayerRefs] = useState({});
+
+    var center, zoom;
+  
 
     const onDistClick = (event) =>{
         // console.log("this works");
@@ -35,29 +39,17 @@ const StateMap = ({selectedState, mapOptions }) => {
     const onEachDist = (dist, layer) => {
         
         var percent = 0;
-        if(selectedState === "maryland"){
-            const objId = dist.properties.OBJECTID;
-            percent = mdPopulation[objId-1].Asian/mdPopulation[objId-1].Total;
-        }else if (selectedState === "virginia"){
-            const objId = dist.properties.ID;
-            percent = vaPopulation[objId-1].Asian/vaPopulation[objId-1].Total;
-        }
+        const objId = dist.properties.OBJECTID || dist.properties.ID;
+        // if(selectedState === "maryland"){
+        //     percent = mdPopulation[objId-1]["American Indian and Alaska Native"]/mdPopulation[objId-1].Total;
+        // }else if (selectedState === "virginia"){
+        //     percent = vaPopulation[objId-1]["American Indian and Alaska Native"]/vaPopulation[objId-1].Total;
+        // }
 
-        var fillcolor = "grey";
-        if(percent < .1){
-            fillcolor = "#caf0f8";
-        }else if(percent >= .1 && percent < .2){
-            fillcolor = "#90e0ef";
-        }else if(percent >= .2 && percent < .3){
-            fillcolor = "#00b4d8";
-        }else if(percent >= .3 && percent < .4){
-            fillcolor = "#0077b6";
-        }else{
-            fillcolor = "#03045e";
-        }
+        setLayerRefs(prevRefs => ({ ...prevRefs, [objId]: layer }));
 
-        layer.options.fillColor = fillcolor;
-        layer.options.fillOpacity = 1;
+        // layer.options.fillColor = determineFillColor(percent);
+        // layer.options.fillOpacity = 1;
 
         layer.on(
             {
@@ -67,6 +59,41 @@ const StateMap = ({selectedState, mapOptions }) => {
             }
         )
     }
+
+    useEffect(() => {
+        Object.keys(layerRefs).forEach(objId => {
+            const layer = layerRefs[objId];
+            if(selectedRace === ""){
+                layer.setStyle({
+                    fillColor: "bluegray",
+                    fillOpacity: 0.3,
+                });
+            }else{
+                let percent = 0;
+                if(selectedState === "maryland"){
+                    percent = mdPopulation[objId-1][selectedRace]/mdPopulation[objId-1].Total;
+                }else if (selectedState === "virginia"){
+                    percent = vaPopulation[objId-1][selectedRace]/vaPopulation[objId-1].Total;
+                }
+                let fillColor = determineFillColor(percent);
+                layer.setStyle({
+                    fillColor: fillColor,
+                    fillOpacity: 1,
+                });
+
+            }
+            
+    });
+    }, [selectedRace, selectedState, layerRefs]);
+
+    const determineFillColor = (percent) => {
+        if(percent < 0.1) return "#caf0f8";
+        else if(percent < 0.2) return "#90e0ef";
+        else if(percent < 0.3) return "#00b4d8";
+        else if(percent < 0.4) return "#0077b6";
+        else return "#03045e";
+    };
+    
 
     if(selectedState === "maryland"){
         center = [38.845753, -76.941273];
@@ -89,9 +116,9 @@ const StateMap = ({selectedState, mapOptions }) => {
                     {selectedState === "virginia" && <GeoJSON color="#03045e" data={VirginiaState.features} onEachFeature={onEachDist}/>}
 
                 </MapContainer>
-                <div style={{position:'absolute', bottom: 0 , right: 0, zIndex:1000, backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: '10px', borderRadius: '5px'}}>
+                {selectedRace !== "" &&<div style={{position:'absolute', bottom: 0 , right: 0, zIndex:1000, backgroundColor: 'rgba(255, 255, 255, 0.8)', padding: '10px', borderRadius: '5px'}}>
                     <Legend legendItems={legendItems}/>
-                </div>
+                </div>}
                 
             </div>
         )
