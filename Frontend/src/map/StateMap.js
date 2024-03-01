27cb97ec7855acdaaf9158f9bef6d3bcd1e2956c
Julie Su <julie.su@stonybrook.edu>
2024-02-29 23:16:55 -0500

    yes

--- Frontend/src/map/StateMap.js
+++ Frontend/src/map/StateMap.js
@@ -5,34 +5,25 @@ import MarylandState from "../Data/District_Boundaries/md_state.json";
 import VirginiaState from "../Data/District_Boundaries/va_state.json";
 import mdPopulation from "../Data/District_Boundaries/md_dummy_data.json";
 import vaPopulation from "../Data/District_Boundaries/va_dummy_data.json"
-import { Component } from 'react';
 import Legend from './Legend';
 import legendItems from './LegendItems';
 
-class StateMap extends Component{
-    constructor(){
-        super();
-        // this.state = {};
-        this.currentState = "virginia";
-        this.center = [39.828175 -98.5795];
-        this.zoom = 7;
-        
+const StateMap = ({selectedState, mapOptions }) => {
+    
+    var center, zoom;
+
+    const onDistClick = (event) =>{
+        // console.log("this works");
     }
 
-    color = ['red', 'green', 'blue', 'yellow', 'orange', 'grey'];
-
-    onDistClick = (event) =>{
-        console.log("this works");
-    }
-
-    onDistHover = (event) =>{
+    const onDistHover = (event) =>{
         event.target.setStyle(
             {
                 color: "orange",
             }
         )
     }
-    mouseLeave = (event) =>{
+    const mouseLeave = (event) =>{
         event.target.setStyle(
             {
                 color: "#03045e",
@@ -40,82 +31,70 @@ class StateMap extends Component{
         )
     }
 
-    onEachDist = (dist, layer) => {
-        console.log(dist.properties);
-        // layer.bindPopup(dist.properties.DISTRICT);
-        // layer.options.fillColor = this.color[Math.floor(5*Math.random())];
-        // layer.options.fillOpacity = Math.random();
-
-        
-
+    const onEachDist = (dist, layer) => {
         
         var percent = 0;
-        if(this.currentState === "maryland"){
+        if(selectedState === "maryland"){
             const objId = dist.properties.OBJECTID;
             percent = mdPopulation[objId-1].Asian/mdPopulation[objId-1].Total;
-        }else if (this.currentState === "virginia"){
+        }else if (selectedState === "virginia"){
             const objId = dist.properties.ID;
             percent = vaPopulation[objId-1].Asian/vaPopulation[objId-1].Total;
         }
 
-        var distOpacity = "grey";
+        var fillcolor = "grey";
         if(percent < .1){
-            distOpacity = "#caf0f8";
+            fillcolor = "#caf0f8";
         }else if(percent >= .1 && percent < .2){
-            distOpacity = "#90e0ef";
+            fillcolor = "#90e0ef";
         }else if(percent >= .2 && percent < .3){
-            distOpacity = "#00b4d8";
+            fillcolor = "#00b4d8";
         }else if(percent >= .3 && percent < .4){
-            distOpacity = "#0077b6";
+            fillcolor = "#0077b6";
         }else{
-            distOpacity = "#03045e";
+            fillcolor = "#03045e";
         }
 
-        layer.options.fillColor = distOpacity;
+        layer.options.fillColor = fillcolor;
         layer.options.fillOpacity = 1;
 
         layer.on(
             {
-                click: this.onDistClick,
-                mouseover: this.onDistHover,
-                mouseout: this.mouseLeave
+                click: onDistClick,
+                mouseover: onDistHover,
+                mouseout: mouseLeave
             }
         )
     }
 
-    // stateStyle = {
-
-    // }
-
-    render(){
-        if(this.currentState === "maryland"){
-            this.center = [38.845753, -76.941273];
-            this.zoom = 8;
-        }else if(this.currentState === "virginia"){
-            this.center = [37.4316, -78.6569];
-            this.zoom = 7;
-        }
-
-        return(
-            <div style={{position:'relative'}}>
-                <MapContainer center={this.center} zoom={this.zoom} scrollWheelZoom={true} minZoom={7} maxZoom={12} style={{display: "inline-block"}}>
-                    <TileLayer
-                        style={{position: 'relative'}}
-                        url="https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png"
-                        // attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    >
-                    </TileLayer>
-                    {this.currentState === "maryland" && <GeoJSON color="#03045e" data={MarylandState.features} onEachFeature={this.onEachDist}/>}
-                    {this.currentState === "virginia" && <GeoJSON color="#03045e" data={VirginiaState.features} onEachFeature={this.onEachDist}/>}
-
-                </MapContainer>
-                <div style={{position:'absolute', bottom:0, right:0, zIndex:1000}}>
-                    <Legend legendItems={legendItems}/>
-                </div>
-                
-            </div>
-        )
+    if(selectedState === "maryland"){
+        center = [38.845753, -76.941273];
+        zoom = 8;
+    }else if(selectedState === "virginia"){
+        center = [37.4316, -78.6569];
+        zoom = 7;
     }
-    
+
+    return(
+        <div style={{position:'relative', width:'50%', height: '70vh', display:'inline-block'}}>
+            <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} minZoom={7} maxZoom={12} style={{display: "inline-block"}}>
+                <TileLayer
+                    style={{position: 'relative'}}
+                    url="https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png"
+                    // attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                >
+                </TileLayer>
+                {selectedState === "maryland" && <GeoJSON color="#03045e" data={MarylandState.features} onEachFeature={onEachDist}/>}
+                {selectedState === "virginia" && <GeoJSON color="#03045e" data={VirginiaState.features} onEachFeature={onEachDist}/>}
+
+            </MapContainer>
+            <div style={{position:'absolute', bottom:0, right:0, zIndex:10000}}>
+                <Legend legendItems={legendItems} />
+            </div>
+            
+
+        </div>
+    )
+
 }
 export default StateMap;
