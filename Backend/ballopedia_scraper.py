af24d8c79676ce5e78047612b36937e55262b60b
rchen279 <ryan.chen.1@stonybrook.edu>
2024-03-14 01:36:34 -0400

    some python stuff

---
+++ Backend/ballopedia_scraper.py
@@ -0,0 +1,30 @@
+# trying to scrape website
+
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://ballotpedia.org/Maryland_House_of_Delegates"
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content,"html.parser")
+
+reps_table = soup.find(id = "officeholder-table")
+resultList = []
+if reps_table:
+  tbody = reps_table.find("tbody")
+  if tbody:
+    tr_els = tbody.find_all('tr')
+    for tr_el in tr_els:
+      row_data = tr_el.find_all('td')
+      temp = []
+      for d in row_data:
+        temp.append(d.text)
+      resultList.append(temp)
+
+print(resultList)
+
+with open("Data/MD_House_Of_Delegates.csv","w") as f:
+    # Iterate over each list in the data
+  for row in resultList:
+      # Join the elements of the list with commas and write to the file
+      f.write(','.join(row) + '\n')
