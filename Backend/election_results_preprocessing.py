e85d9235c6d8bd01214fdfc97553c3d7f988bf3f
rchen279 <ryan.chen.1@stonybrook.edu>
2024-03-16 02:03:06 -0400

    virginia election results data

--- Backend/election_results_preprocessing.py
+++ Backend/election_results_preprocessing.py
@@ -1,54 +1,46 @@
+import json
 import pandas as pd
-
+import pprint
 df = pd.read_csv("Data/Virginia_Election_Results_Raw.csv")
-
-# print(df.head)
-# print('------------------------------------------------------------------------------')
-# print(df.columns)
-# print("---------------------------------------------------------------------------")
-# print("the precinct id series...")
-
-
-# campbell = df[df['LocalityName'] == "CAMPBELL COUNTY"]
-# uni_precinct_ids = campbell["PrecinctId"].unique()
-# for pid in uni_precinct_ids:
-#   onepid = campbell[campbell["PrecinctId"] == pid]
-#   print(onepid)
-#   # print(onepid["CandidateId"].unique())
-#   break
+pd.set_option('display.max_rows', None)
 
 # filter by election type - House of Delegates
 df = df[df["OfficeTitle"].str.contains("House of Delegates")]
-
-# 'CandidateName','LocalityName','OfficeTitle'
-# print(house_of_delegates_df[['CandidateName','LocalityName','OfficeTitle']])
+assert len(df['OfficeTitle'].unique()) == 100 # check 100 districts
 
 all_districts = df["OfficeTitle"].unique()
-# look at unique values in office title
-listcols = ['CandidateName','TOTAL_VOTES','PrecinctId','LocalityName','OfficeTitle']
-pd.set_option('display.max_rows', None)
-for district in all_districts:
-  print(f"Race ---> {district}")
-  print()
-  num_rows_in_race = df[df["OfficeTitle"]==district][listcols].shape[0]
+sampleCols = ['CandidateName','TOTAL_VOTES','PrecinctId','LocalityName','OfficeTitle']
 
-  print(df[df["OfficeTitle"]==district][listcols])
-  # for given district, filter by county
-  print("\n\nthe counties that make up the district include ...\n\n\n")
-  counties_in_district = df[df["OfficeTitle"]==district]['LocalityName'].unique()
-  print(counties_in_district,"\n\n")
+virginia_election_data = {}
 
-  # for given county, look at precincts
-  for county in counties_in_district:
-    temp = df[df["OfficeTitle"]==district]
-    # print("\n\n\nboobarb\n")
-    county_df = temp[temp['LocalityName'] == county]
-    # print(county_df[listcols])
-    
-    # for each county, analyze precincts
-    county_specific_precincts = county_df["PrecinctId"].unique()
-    for precinct in county_specific_precincts:
-      print(county_df[county_df["PrecinctId"]== precinct][listcols])
+i = 0
+for district in all_districts: # each assembly district race
+  print(f"District ----------- {district}")
+  # get district number
+  district_num = ''.join([char for char in district if char.isdigit()])
+  virginia_election_data[district_num] = {}
+  district_df = df[df["OfficeTitle"]==district]
+  all_counties = district_df['LocalityName'].unique()
+  for county in all_counties:
+    virginia_election_data[district_num][county] = {}
+    county_df = district_df[district_df['LocalityName'] == county]
+    all_precincts = county_df['PrecinctId'].unique()
+    for precinct in all_precincts:
+      precinct_level_data = {'Precinct_Name': "", 'Election_Results': {}} # precinct specific data
+      precinct_df = county_df[county_df['PrecinctId'] == precinct]
+      precinct_name = ""
+      for index, row in precinct_df.iterrows():
+        # print(row['CandidateName'], row['TOTAL_VOTES'], row['PrecinctId'], row['LocalityName'],row['OfficeTitle'])
+        precinct_name = row['PrecinctName']
+        precinct_level_data['Election_Results'][row['CandidateName']] = row['TOTAL_VOTES']
+      precinct_level_data['Precinct_Name'] = precinct_name
+      virginia_election_data[district_num][county][precinct] = precinct_level_data
+  print("======================================================================")
 
-  print('---------------------------------------------------------------------------')
-  break
+  # i+=1
+  # if i == 5:
+  #   break
+
+pprint.pprint(virginia_election_data)
+with open('Data/virginia_election_data.json', 'w') as json_file:
+    json.dump(virginia_election_data, json_file, indent=4)
