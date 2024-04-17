cb8ba991663ed8d461e283a5a6237402d18c98db
rchen279 <ryan.chen.1@stonybrook.edu>
2024-04-16 20:06:56 -0400

    final draft

--- Backend/Virginia_Preprocessing_Aggregation.py
+++ Backend/Virginia_Preprocessing_Aggregation.py
@@ -48,29 +48,34 @@ for obj in Virginia_Result_Data['features']:
     if matchingPrecinct:
       # add to aggregate result, remove from sets
       # print("Successful")
-      obj['properties']['Demographic_Data'] = dem_data
-      del VA_Demographic_Data[iden]
 
-# pprint.pprint(Virginia_Result_Data)
+      # obj['properties']['Demographic_Data'] = dem_data
+      obj['properties']['registered_voters_european'] = dem_data['European']
+      obj['properties']['registered_voters_hispanic'] = dem_data['Hispanic']
+      obj['properties']['registered_voters_african_american'] = dem_data['AfricanAmerican']
+      obj['properties']['registered_voters_south_east_asian'] = dem_data['SouthEastAsian']
+      obj['properties']['registered_voters_other'] = dem_data['Other']
+      obj['properties']['registered_voters_unknown'] = dem_data['Unknown']
+      obj['properties']['registered_voters_total'] = dem_data['Total']
+
+      del VA_Demographic_Data[iden]
 
 count_has_demographic_info=0
 count_missing_demographic_info=0
 for item in Virginia_Result_Data['features']:
   props=item['properties']
-  if 'Demographic_Data' in props:
+  if 'registered_voters_european' in props:
     count_has_demographic_info+=1
   else:
     count_missing_demographic_info+=1
     # fill with NA
-    item['properties']['Demographic_Data'] = {
-      "European": 0,
-      "Hispanic": 0,
-      "AfricanAmerican": 0,
-      "SouthEastAsian": 0,
-      "Other": 0,
-      "Unknown": 0,
-      "Total": 0
-    }
+    item['properties']['registered_voters_european'] = 0
+    item['properties']['registered_voters_hispanic'] = 0
+    item['properties']['registered_voters_african_american'] = 0
+    item['properties']['registered_voters_south_east_asian'] = 0
+    item['properties']['registered_voters_other'] = 0
+    item['properties']['registered_voters_unknown'] = 0
+    item['properties']['registered_voters_total'] = 0
 
 print("Results from adding demographic information")
 print("Successfully added: ",count_has_demographic_info)
@@ -81,10 +86,14 @@ print("Missing Total: ",count_missing_demographic_info)
 print("After filling in missing dem data")
 for item in Virginia_Result_Data['features']:
   props=item['properties']
-  assert 'Demographic_Data' in props
+  assert "registered_voters_european" in props
+  assert "registered_voters_hispanic" in props
+  assert "registered_voters_african_american" in props
+  assert "registered_voters_south_east_asian" in props
+  assert "registered_voters_other" in props
+  assert "registered_voters_unknown" in props
+  assert "registered_voters_total" in props
 print("Successful insertion of 0 values")
-
-
 
 
 # merge Virginia_Result_Data with election results data
@@ -92,23 +101,23 @@ VA_Election_Results_Data = None
 with open('Data/va_precinct_election_results.json','r') as file:
   VA_Election_Results_Data=json.load(file)
 
-# for unique_id,election_results in election_results_deep_copy.items():
-#   match = re.findall(r'\((\d+),\s*(\d+)\)',unique_id)
-#   if len(match) == 0:
-#     print("error detected with match",unique_id)
-#     continue
-#   match = match[0]
-#   county_fips_code_1, vtd_id_1 = int(match[0]),int(match[1])
-#   # print(county_fips_code_1,vtd_id_1)
-#   # lookup precinct entry in 
-#   key = str((county_fips_code_1,vtd_id_1))
-#   precinct_entry = Virginia_Result_Data.get(key)
-#   if precinct_entry:
-#     print("found!!")
-#     Virginia_Result_Data.get(key)['Election_Data'] = election_results
-#     del VA_Election_Results_Data[unique_id]
-#   else:
-#     print("Precinct not found...",key)
+# # for unique_id,election_results in election_results_deep_copy.items():
+# #   match = re.findall(r'\((\d+),\s*(\d+)\)',unique_id)
+# #   if len(match) == 0:
+# #     print("error detected with match",unique_id)
+# #     continue
+# #   match = match[0]
+# #   county_fips_code_1, vtd_id_1 = int(match[0]),int(match[1])
+# #   # print(county_fips_code_1,vtd_id_1)
+# #   # lookup precinct entry in 
+# #   key = str((county_fips_code_1,vtd_id_1))
+# #   precinct_entry = Virginia_Result_Data.get(key)
+# #   if precinct_entry:
+# #     print("found!!")
+# #     Virginia_Result_Data.get(key)['Election_Data'] = election_results
+# #     del VA_Election_Results_Data[unique_id]
+# #   else:
+# #     print("Precinct not found...",key)
 
 for obj in Virginia_Result_Data['features']:
   # obj['properties']["foobar"] = "foofoo"
@@ -124,7 +133,15 @@ for obj in Virginia_Result_Data['features']:
   if key in VA_Election_Results_Data:
     # print("detected..")
     # print(key,VA_Election_Results_Data[key])
-    obj['properties']['Election_Data'] = VA_Election_Results_Data[key]
+    # obj['properties']['Election_Data'] = VA_Election_Results_Data[key]
+    # print(VA_Election_Results_Data[key])
+
+    obj['properties']['precinct_name'] = VA_Election_Results_Data[key]['precinct_name']
+    obj['properties']['district_id'] = VA_Election_Results_Data[key]['district_id']
+    obj['properties']['election_votes_for_dem_cand'] = VA_Election_Results_Data[key]['votes_for_dem_cand']
+    obj['properties']['election_votes_for_rep_cand'] = VA_Election_Results_Data[key]['votes_for_rep_cand']
+    obj['properties']['election_votes_for_other_cand'] = VA_Election_Results_Data[key]['votes_for_other_cand']
+    obj['properties']['election_votes_total'] = VA_Election_Results_Data[key]['votes_total']
 
 
 print("Results from adding election information")
@@ -132,21 +149,34 @@ successful_election_additions=0
 failed_election_additions=0
 for item in Virginia_Result_Data['features']:
   props=item['properties']
-  if 'Election_Data' in props:
+  if 'precinct_name' in props:
     successful_election_additions += 1
   else:
     failed_election_additions +=1
-    item['properties']['Election_Data'] = {
-      "precinct_name": 0,
-      "district_id": 0,
-      "votes_for_dem_cand": 0,
-      "votes_for_rep_cand": 0,
-      "votes_for_other_cand": 0,
-      "votes_total": 0
-    }
+    item['properties']['precinct_name'] = 0
+    item['properties']['district_id'] = 0
+    item['properties']['election_votes_for_dem_cand'] = 0
+    item['properties']['election_votes_for_rep_cand'] = 0
+    item['properties']['election_votes_for_other_cand'] = 0
+    item['properties']['election_votes_total'] = 0
+
+    # print("couldn't find",item['properties'])
+
+# assert election data in there
+for item in Virginia_Result_Data['features']:
+  props=item['properties']
+  assert 'precinct_name' in props
+  assert 'district_id' in props
+  assert 'election_votes_for_dem_cand' in props
+  assert 'election_votes_for_rep_cand' in props
+  assert 'election_votes_for_other_cand' in props
+  assert 'election_votes_total' in props
+
+
 print("Successful additions: {}\n, Failed additions (flled with 0): {}".format(
   successful_election_additions, failed_election_additions))
 
+# pprint.pprint(Virginia_Result_Data)
 
 # write json to file
 with open("Data/Virgina_Result_Data", "w") as json_file:
