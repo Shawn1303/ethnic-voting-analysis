d00971bdbfc2e5eda5eeff6140beb8d2d6d40398
Julie Su <julie.su@stonybrook.edu>
2024-05-12 08:45:07 -0400

    ei

--- Frontend/src/Components/Pages/Ei.js
+++ Frontend/src/Components/Pages/Ei.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import { DensityChart } from "./Page2Components/DensityChart";
-import {data} from './Page2Components/data';
+import data from './Page2Components/combined_data.json'
 
-// const data = [
-// 	75.0,
-// 	104.0,
-// 	369.0,
-// 	300.0,
-// 	92.0
-// ]
 export default function Ei(props) {
     
 	return(
 		<Box margin={'10px 1%'}>
-			<Grid container spacing={1}>
-				<Grid item xs={12} style={{height: '40vh', overflowY: 'auto'}}>
-                    <DensityChart width={1200} height={300} data={data} />
-				</Grid>
-				<Grid item xs={12} style={{height: '40vh', overflowY: 'auto'}}>
-					<DensityChart width={1200} height={300} data={data} />
-				</Grid>	
-			</Grid>
+			{ props.state &&
+				<>
+				<Typography style={{ transform: [{ rotate: "90deg" }]}}>Probability Density</Typography>
+				<Grid container spacing={1}>
+					<Grid item xs={12} style={{height: '29vh', overflowY: 'auto'}}>
+						<DensityChart width={1200} height={220} data={data[0]} />
+					</Grid>
+					<Grid item xs={12} style={{height: '29vh', overflowY: 'auto'}}>
+						<DensityChart width={1200} height={220} data={data[2]} />
+					</Grid>	
+					<Grid item xs={12} style={{height: '29vh', overflowY: 'auto'}}>
+						<DensityChart width={1200} height={220} data={data[5]} />
+					</Grid>	
+				</Grid></>
+			}
 		</Box>
 	)
 }
--- Frontend/src/Components/Pages/Page2Components/DensityChart.js
+++ Frontend/src/Components/Pages/Page2Components/DensityChart.js
@@ -1,64 +1,68 @@
 import { useMemo } from "react";
 import * as d3 from "d3";
 import { AxisBottom } from "./AxisBottom";
+import { AxisLeft } from "./AxisLeft";
 
-const MARGIN = { top: 30, right: 10, bottom: 50, left: 10 };
+const MARGIN = { top: 10, right: 10, bottom: 50, left: 60 };
+const COLORS = ['#e0ac2b', '#e85252', '#6689c6', '#9a6fb0', '#a53253'];
+
 
 export const DensityChart = ({ width, height, data }) => {
   const boundsWidth = width - MARGIN.right - MARGIN.left;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
-  console.log(data)
+
+  const allGroupRace = data.data.map((group) => group.race);
+  // console.log(allGroupRace)
+  const colorScale = d3.scaleOrdinal().domain(allGroupRace).range(COLORS);
+  // console.log(data)
   const xScale = useMemo(() => {
     // Using a fixed domain for xScale as an example
     return d3.scaleLinear()
-      .domain([0, 1000]) // Fixed domain
+      .domain([0, 1]) // Fixed domain
       .range([0, boundsWidth]);
   }, [width]);
 
-  // Compute kernel density estimation
-  const density = useMemo(() => {
-    const kde = kernelDensityEstimator(kernelEpanechnikov(7), xScale.ticks(40));
-    return kde(data);
-  }, [xScale, data]);
+  const densityGenerator = kernelDensityEstimator(kernelEpanechnikov(0.005), xScale.ticks(100));
+  const densityData = data.data.map(group => ({
+    name: group.race,
+    density: densityGenerator(group.value),
+  }));
 
-  const yScale = useMemo(() => {
-    const max = Math.max(...density.map(d => d[1]));
-    return d3.scaleLinear()
-      .range([boundsHeight, 0])
-      .domain([0, max]);
-  }, [density]);
-
-  const path = useMemo(() => {
-    const lineGenerator = d3.line()
-      .x(d => {
-        console.log('xScale input:', d[0], 'output:', xScale(d[0]));
-        return xScale(d[0]);
-      })
-      .y(d => {
-        console.log('yScale input:', d[1], 'output:', yScale(d[1]));
-        return yScale(d[1]);
-      })
-      .curve(d3.curveBasis);
-    return lineGenerator(density);
-  }, [density, xScale, yScale]);
+  // const yScale = useMemo(() => {
+  //   const max = Math.max(...density.map(d => d[1]));
+  //   return d3.scaleLinear()
+  //     .range([boundsHeight, 0])
+  //     .domain([0, max]);
+  // }, [densityData]);
+  const yMax = Math.max(...densityData.flatMap(group => group.density.map(d => d[1])));
+  const yScale = useMemo(() => d3.scaleLinear().domain([0, yMax]).range([boundsHeight, 0]), [yMax]);
+  // const path = useMemo(() => {
+  //   const lineGenerator = d3.line()
+  //     .x(d => {
+  //       console.log('xScale input:', d[0], 'output:', xScale(d[0]));
+  //       return xScale(d[0]);
+  //     })
+  //     .y(d => {
+  //       console.log('yScale input:', d[1], 'output:', yScale(d[1]));
+  //       return yScale(d[1]);
+  //     })
+  //     .curve(d3.curveBasis);
+  //   return lineGenerator(density);
+  // }, [density, xScale, yScale]);
+  const pathGenerator = d3.line().x(d => xScale(d[0])).y(d => yScale(d[1])).curve(d3.curveBasis);
+  const allShapes = densityData.map((group, i) => (
+    <path key={i} d={pathGenerator(group.density)} fill={colorScale(group.name)} opacity={0.4} stroke="black" strokeWidth={1} strokeLinejoin="round" />
+  ));
 
   return (
     <svg width={width} height={height}>
-      <g
-        transform={`translate(${MARGIN.left}, ${MARGIN.top})`}
-      >
-        <path
-          d={path}
-          fill="#9a6fb0"
-          opacity="0.4"
-          stroke="black"
-          strokeWidth="1"
-          strokeLinejoin="round"
-        />
-
-        {/* X axis */}
+      <g transform={`translate(${MARGIN.left},${MARGIN.top})`}>
+        {allShapes}
+        <AxisLeft yScale={yScale} pixelsPerTick={30} />
+        
         <g transform={`translate(0, ${boundsHeight})`}>
           <AxisBottom xScale={xScale} pixelsPerTick={40} />
+          
         </g>
       </g>
     </svg>
